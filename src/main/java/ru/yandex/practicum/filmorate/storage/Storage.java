4439576db635c5feecb3e49a35bece7b220d7990
Michael <kinovar13@gmail.com>
2026-07-24 03:51:28 +0700

    checkstyle corrections

--- src/main/java/ru/yandex/practicum/filmorate/storage/Storage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/Storage.java
@@ -3,9 +3,12 @@ package ru.yandex.practicum.filmorate.storage;
 import java.util.List;
 import java.util.Optional;
 
-public interface Storage <T>{
+public interface Storage<T> {
     T create(T element);
+
     T update(T element);
+
     List<T> getAll();
+
     Optional<T> findById(Long id);
 }
