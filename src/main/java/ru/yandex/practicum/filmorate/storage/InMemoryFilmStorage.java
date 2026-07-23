d16ecd8d08dda5ca69e26e3b37957a949cd31418
Michael <kinovar13@gmail.com>
2026-07-24 03:55:59 +0700

    checkstyle corrections

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -10,36 +10,36 @@ import java.util.*;
 @Component
 
 public class InMemoryFilmStorage implements FilmStorage {
-   private final Map<Long, Film> films = new HashMap<>();
-   private long nextId = 1;
+    private final Map<Long, Film> films = new HashMap<>();
+    private long nextId = 1;
 
 
-   @Override
-   public Optional<Film> findById(Long id) {
-      if (films.containsKey(id)) {
-         return Optional.of(films.get(id));
-      }
-      return Optional.empty();
-   }
+    @Override
+    public Optional<Film> findById(Long id) {
+        if (films.containsKey(id)) {
+            return Optional.of(films.get(id));
+        }
+        return Optional.empty();
+    }
 
 
-   @Override
-   public Film create(Film film) {
-      film.setId(nextId++);
-      films.put(film.getId(), film);
-      return film;
-   }
+    @Override
+    public Film create(Film film) {
+        film.setId(nextId++);
+        films.put(film.getId(), film);
+        return film;
+    }
 
-   @Override
-   public Film update(Film film) {
+    @Override
+    public Film update(Film film) {
 
-      films.put(film.getId(), film);
+        films.put(film.getId(), film);
 
-      return film;
-   }
+        return film;
+    }
 
-   @Override
-   public List<Film> getAll() {
-      return new ArrayList<>(films.values());
-   }
+    @Override
+    public List<Film> getAll() {
+        return new ArrayList<>(films.values());
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -2,5 +2,5 @@ package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.User;
 
-public interface UserStorage extends Storage<User>{
+public interface UserStorage extends Storage<User> {
 }
